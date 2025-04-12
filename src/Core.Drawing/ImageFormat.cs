e99973735fedb6b44dd66908b87fc456c56c6ec1
Cédric Belin <cedric@belin.io>
2025-04-12 10:43:27 +0200

    Add the `Belin.Core.Drawing` assembly

---
+++ src/Core.Drawing/ImageFormat.cs
@@ -0,0 +1,32 @@
+namespace Belin.Core.Drawing;
+
+/// <summary>
+/// Defines the format of an image.
+/// </summary>
+public enum ImageFormat {
+
+	/// <summary>
+	/// The image format is AV1 Image File Format (AVIF).
+	/// </summary>
+	Avif,
+
+	/// <summary>
+	/// The image format is Graphics Interchange Format (GIF).
+	/// </summary>
+	Gif,
+
+	/// <summary>
+	/// The image format is Joint Photographic Experts Group (JPEG).
+	/// </summary>
+	Jpeg,
+
+	/// <summary>
+	/// The image format is Portable Network Graphics (PNG).
+	/// </summary>
+	Png,
+
+	/// <summary>
+	/// The image format is WebP.
+	/// </summary>
+	WebP
+}
