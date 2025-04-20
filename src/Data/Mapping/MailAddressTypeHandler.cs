99d8230856528701f4f1d0f397592f4ddece7186
Cédric Belin <cedric@belin.io>
2025-04-20 14:41:57 +0200

    Add the `MailAddressTypeHandler` class

--- src/Data/Mapping/MailAddressTypeHandler.cs
+++ src/Data/Mapping/MailAddressTypeHandler.cs
@@ -1,26 +1,26 @@
 namespace Belin.Core.Data.Mapping;
 
 using System.Data;
-using System.Net;
+using System.Net.Mail;
 
 /// <summary>
-/// Maps an Internet Protocol (IP) address to or from a string.
+/// Maps a mail address to or from a string.
 /// </summary>
-public class IPAddressTypeHandler: SqlMapper.TypeHandler<IPAddress> {
+public class MailAddressTypeHandler: SqlMapper.TypeHandler<MailAddress> {
 
 	/// <summary>
 	/// Assigns the value of a parameter before a command executes.
 	/// </summary>
 	/// <param name="parameter">The parameter to configure.</param>
 	/// <param name="value">The parameter value.</param>
-	public override void SetValue(IDbDataParameter parameter, IPAddress? value) =>
-		parameter.Value = value?.MapToIPv6().ToString();
+	public override void SetValue(IDbDataParameter parameter, MailAddress? value) =>
+		parameter.Value = value?.Address;
 
 	/// <summary>
 	/// Parses a database value back to a typed value.
 	/// </summary>
 	/// <param name="value">The value from the database.</param>
 	/// <returns>The typed value.</returns>
-	public override IPAddress? Parse(object value) =>
-		value is string ipString && !string.IsNullOrWhiteSpace(ipString) ? IPAddress.Parse(ipString) : null;
+	public override MailAddress? Parse(object value) =>
+		value is string address && !string.IsNullOrWhiteSpace(address) ? new MailAddress(address) : null;
 }
